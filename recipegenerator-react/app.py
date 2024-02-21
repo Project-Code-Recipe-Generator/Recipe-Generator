657b7ae290678f4204f4f9d7816f6c59939caa1f
saada4 <saada4@illinois.edu>
2024-02-20 21:30:12 -0600

    Basic Flask App Structure
    Made basic structure of the Flask application
    just do "flask run" or "python3 app.py", and will run.

---
+++ recipegenerator-react/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from app_resources import all_views
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_pyfile('./app/config.py', silent=True)
+
+    app.register_blueprint(all_views)
+
+    return app
+
+app: Flask = create_app()
+
+# serve
+if __name__ == '__main__':
+    app.run(host=app.config["API_SERVER_HOST"], port=app.config["API_SERVER_PORT"], debug=True)
+
