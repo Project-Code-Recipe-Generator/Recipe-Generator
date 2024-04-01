1f77f000b93680c8e2e0baf70f24900587f68541
shirleyma04 <shirleyma2004@gmail.com>
2024-04-01 17:43:00 -0500

    Top part of Recipe page

--- recipegenerator-react/src/components/Recipe.jsx
+++ recipegenerator-react/src/components/Recipe.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Recipe = ({ recipe }) => {
   return (
     <div>
+      <h1>Recipe Generator</h1>
       <h2>{recipe.name}</h2>
       {/* Render the rest of the recipe details here */}
     </div>
