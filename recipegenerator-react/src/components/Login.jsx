0d1daa0cfe026490131d04154c8374ef719beace
shirleyma04 <shirleyma2004@gmail.com>
2024-04-02 22:21:49 -0500

    UPDATED login page

--- recipegenerator-react/src/components/Login.jsx
+++ recipegenerator-react/src/components/Login.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <div className="box">
-        <label for="inputBox">Username:</label>
-        <input type="text" id="inputBox" name="inputBox" />
-        <label for="inputBox">Password:</label>
-        <input type="text" id="inputBox" name="inputBox" />
+        <label htmlFor="username" className="input-text">
+          Username:
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Type here..."
+          className="input-box"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password" className="input-text">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Type here..."
+          className="input-box"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="button">Log In</button>
       </div>
     </div>
